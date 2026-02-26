a06150203681d19f7aec1f4a19a180be196481be
Vullnet02 <vullnetjakupi3@gmail.com>
2026-02-26 15:27:03 +0100

    update i ri

--- script.js
+++ script.js
@@ -3,15 +3,7 @@ let count = 10;
 btnClick.addEventListener("click", function () {
   count = count + 10;
   text.innerText = "Ti i ki " + count + " Kaqika n kfc";
-    document.body.style.backgroundColor = "purple";
-    document.getElementById("div1").style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Flag_of_Kosovo.svg/3840px-Flag_of_Kosovo.svg.png')";
-    document.getElementById("div2").style.backgroundColor = "green";
-    document.getElementById("div1").style.color = "white";
-    document.getElementById("div2").style.color = "white";
-    document.getElementById("div1").style.fontSize = "20px";
-    document.getElementById("div2").style.fontSize = "20px";
-    document.getElementById("div1").style.padding = "20px";
-    document.getElementById("div2").style.padding = "20px";
+  
 
 });
 
